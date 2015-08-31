bbdad545c058e1fd1dc26eb85fb8977be8e1cedc
dorrianmatthew1712 <dorrianmatthew@gmail.com>
2015-08-31 20:42:18 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shark and Shrimp Object
+            Shark cShark = new Shark();
+            Shrimp lenny = new Shrimp();
+
+            //Call the Eat method
+            cShark.Eat();
+            lenny.Eat();
+        }
+    }
+}
---
+++ Shark.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    //Implements the Animal Interface
+    public class Shark : Animal
+    {
+        //Implementation of the Eat method declared in the interface
+        public void Eat()
+        {
+            Console.WriteLine("I love eating fish, seals, and anything meaty!");
+        }
+    }
+}
